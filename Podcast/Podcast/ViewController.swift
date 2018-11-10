a8de6a4f5fb018f98bcc25c2764e1e751477bef9
Kylta <Christophe.bugnon02@gmail.com>
2018-11-10 12:58:13 +0100

    Create Podcast project + add Swiftlint

---
+++ Podcast/Podcast/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Christophe Bugnon on 10/11/2018.
+//  Copyright © 2018 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
